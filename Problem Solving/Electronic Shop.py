7d1759f88de8452b3d15185a765986e2ea8aa580
AshwinAshok3 <149888800+AshwinAshok3@users.noreply.github.com>
2025-03-17 19:16:37 +0530

    Create Electronic Shop.py

---
+++ Problem Solving/Electronic Shop.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    valid_sums = []  # Create an empty list
+    for x in keyboards:
+        for y in drives:
+            total = x + y
+            if total <= b:  # Only store if within budget
+                valid_sums.append(total)
+
+    valid_sums.append(-1)  # Ensures max() never fails
+    return max(valid_sums)  # Get the maximum valid sum
+            
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
